8b4be62dfa1c4d33a988dd2309971302ac8e3217
Daarkozzy <107293198+Daarkozzy@users.noreply.github.com>
2024-09-06 20:43:01 -0300

    Atualizar o script.js

--- src/script.js
+++ src/script.js
@@ -2,22 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const animeButton = document.getElementById('anime-button');
     const animeContainer = document.getElementById('anime-container');
     const genreSelect = document.getElementById('genre-select');
-
+    const loadingSpinner = document.getElementById('loadingSpinner');
 
     async function fetchRandomAnimeWithFilter(genre) {
         try {
             let anime;
             do {
-                // Fetch a random anime from the Jikan API
+            
                 const response = await fetch('https://api.jikan.moe/v4/random/anime');
                 const data = await response.json();
                 anime = data.data;
 
-                // Check if the anime matches the selected genre
                 if (genre && !anime.genres.some(g => g.name.toLowerCase() === genre.toLowerCase())) {
-                    anime = null; // Não exibir se o gênero não corresponder
+                    anime = null; 
                 }
-            } while (!anime || anime.type !== 'TV'); // Repetir até encontrar um anime do tipo "TV" e do gênero correto
+            } while (!anime || anime.type !== 'TV'); 
 
             return anime;
         } catch (error) {
@@ -26,21 +25,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     async function showRandomAnime() {
+        
+        loadingSpinner.style.display = 'block';
+        animeContainer.innerHTML = ''; 
+
         const selectedGenre = genreSelect.value;
         const anime = await fetchRandomAnimeWithFilter(selectedGenre);
+        
         if (anime) {
             animeContainer.innerHTML = `
                 <h2>${anime.title}</h2>
                 <img src="${anime.images.jpg.large_image_url}" alt="${anime.title}" style="max-width: 100%; height: 300px;">
             `;
         }
+        
+        
+        loadingSpinner.style.display = 'none';
     }
 
     animeButton.addEventListener('click', showRandomAnime);
 });
 
 
-// Navbar Menu code
 const navbarMenu = document.getElementById("menu");
 const burgerMenu = document.getElementById("burger");
 
